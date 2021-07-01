050ec17ccf8e71627cfe8b376403797efe8e8a8a
dazzlingstreak <dazzlingstreak@hotmail.com>
2021-07-01 15:39:05 +0800

    initial commit

---
+++ src/main/java/com/example/all/ChaosAllApplication.java
@@ -0,0 +1,13 @@
+package com.example.all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaosAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaosAllApplication.class, args);
+    }
+
+}
